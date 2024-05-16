0777d5e442fe593a5b183e8e9868d5b1aae86dcf
Loïc Guitaut <loic@discourse.org>
2024-05-16 17:36:05 +0200

    DEV: Update rubocop-discourse to latest version

---
+++ lib/discourse_no_bump/no_bump_validator.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module DiscourseNoBump
+  class NoBumpValidator < ActiveModel::Validator
+    def validate(record)
+      return unless SiteSetting.no_bump_enabled?
+      return if record.topic.private_message?
+      return if record.topic.user_id != record.user_id
+      return if record.user.staff? || record.user.trust_level > SiteSetting.no_bump_trust_level
+
+      last_post_user_id =
+        Post
+          .with_deleted
+          .where(topic_id: record.topic_id)
+          .order("post_number desc")
+          .limit(1)
+          .pluck(:user_id)
+          .first
+
+      if last_post_user_id == record.user_id
+        record.errors.add(:base, message: I18n.t("no_bump_error"))
+      end
+    end
+  end
+end
